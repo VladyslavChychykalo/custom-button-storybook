98ab4717da3c30607ac29586885a0f85a61b15e6
Vladyslav <vladyslav.chychykalo@gmail.com>
2021-08-10 15:59:18 +0300

    initial commit with storybook

---
+++ src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,10 @@
+import s from './CustomeButton.module.scss'
+import Icon from ''
+
+const CustomButton = ({ text, url }) => (
+    <button>{url && }</button>
+);
+
+export default CustomButton;
+
+// primary (boolean)
